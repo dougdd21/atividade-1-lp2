77deffca4468413c5c10ddf8c8709661687470e2
dougdd21 <douglasmateus827@gmail.com>
2022-02-21 00:28:08 -0300

    iniciando a resolucao final da atividade

---
+++ estudante.java
@@ -0,0 +1,23 @@
+
+
+public class estudante {
+    double nota1, nota2, nota3, media, notaFaltante;
+    String nomeAluno;
+    
+    
+
+    
+    public void mediaAluno(){
+        media = nota1 + nota2 + nota3;
+    }
+    
+    
+    public void desempenhoAluno(){
+        if (media >= 60){
+            System.out.println(nomeAluno+" você esta APROVADO, sua nota é:"+media);
+            }
+            else{
+                System.out.println(nomeAluno+" você esta REPROVADO, sua nota é:"+media+" , faltam"
+                +notaFaltante+"para voce passar");
+            }
+}
